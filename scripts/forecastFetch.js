4a9d8d7f3b12dad3202c5a858b7b722a078a0642
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-12-02 09:02:29 -0500

    flush out fetch page

---
+++ scripts/forecastFetch.js
@@ -0,0 +1,18 @@
+import {cityDatabase} from './citiesDatabase'
+
+window.onload = () =>{
+
+    // fetch the data from the endpoint 
+    // turn that data into a json object and create a data function 
+
+    //when a city is selected i want the log and lat 
+
+    //than i want to find the log and lat in the api 
+
+    //open the api data and displace
+   fetch()
+    if(cityDatabase.longitude,cityDatabase.latitude ==  selectedCity.latitude, selectedCity.longitude){
+        let stationLookupUrl =`https://api.weather.gov/points/${selectedCity.latitude},${selectedCity.longitude}`;
+    }
+    
+}
