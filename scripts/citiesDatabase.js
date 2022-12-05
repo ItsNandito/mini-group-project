765fa55ba9d2f8309431aca77ad5ef3daf967c07
Fernando criollo <fernando.e.criollo@gmail.com>
2022-12-04 19:40:28 -0500

    fixed database syntax error

--- scripts/citiesDatabase.js
+++ scripts/citiesDatabase.js
@@ -1,52 +1,52 @@
-let cityDatabase = [
-  {
-    name: "Tampa, FL",
-    latitude: 27.995776,
-    longitude: -82.451116,
-  },
-  {
-    name: "Myrtle Beach, SC",
-    latitude: 33.686978,
-    longitude: -78.884495,
-  },
-  {
-    name: "Jacksonville, FL",
-    latitude: 30.327304,
-    longitude: -81.655712,
-  },
-  {
-    name: "St. Petersburg, FL",
-    latitude: 27.777385,
-    longitude: -82.659492,
-  },
-  {
-    name: "Miami, FL",
-    latitude: 25.773896,
-    longitude: -80.207687,
-  },
-  {
-    name: "Raleigh, NC",
-    latitude: 35.779656,
-    longitude: -78.640047,
-  },
-  {
-    name: "Richmand, VA",
-    latitude: 37.506016,
-    longitude: -77.433146,
-  },
-  {
-    name: "Philadlphia, PA",
-    latitude: 27.995776,
-    longitude: -75.163584,
-  },
-  {
-    name: "Central Park, NY",
-    latitude: 40.78126,
-    longitude: -73.966529,
-  },
-  {
-    name: "San Francisco, CA",
-    latitude: 37.75616,
-    longitude: -122.441104,
-  },
-];
+export let cityDatabase = [
+    {
+        name: "Knoxville, TN",
+        latitude: 35.964668,
+        longitude: -83.926453
+    },
+    {
+        name: "Myrtle Beach, SC",
+        latitude: 33.686978,
+        longitude: -78.884495
+    },
+    {
+        name: "Jacksonville, FL",
+        latitude: 30.327304,
+        longitude: -81.655712
+    },
+    {
+        name: "St. Petersburg, FL",
+        latitude: 27.777385,
+        longitude: -82.659492
+    },
+    {
+        name: "Miami, FL",
+        latitude: 25.773896,
+        longitude: -80.207687
+    },
+    {
+        name: "Raleigh, NC",
+        latitude: 35.779656,
+        longitude: -78.640047
+    },
+    {
+        name: "Richmand, VA",
+        latitude: 37.506016,
+        longitude: -77.433146
+    },
+    {
+        name: "Philadlphia, PA",
+        latitude: 27.995776,
+        longitude: -75.163584
+    },
+    {
+        name: "Central Park, NY",
+        latitude: 40.781260,
+        longitude: -73.966529
+    },
+    {
+        name: "San Francisco, CA",
+        latitude: 37.75616,
+        longitude: -122.441104
+    }
+]
